5f8ac248e9726ae6b415e169ad6714501f74acfd
QuietRise <micro859775784@outlook.com>
2025-11-02 10:31:02 +0800

    Fixed the wrong code placement.

--- function.c
+++ function.c
@@ -1,5 +1,16 @@
 #include "function.h"
 
-double rectangleArea( rectangle *r);
-double rectanglePerimeter( rectangle *r);
-double rectangleDiagonal( rectangle *r);
+double rectangleArea( rectangle *r)
+{
+    return fabs(r->point_1.x-r->point_2.x) * fabs(r->point_1.y-r->point_2.y);
+}
+
+double rectanglePerimeter( rectangle *r)
+{
+    return 2 * ( fabs(r->point_1.x-r->point_2.x) + fabs(r->point_1.y-r->point_2.y));
+}
+
+double rectangleDiagonal( rectangle *r)
+{
+    return hypot( (r->point_1.x-r->point_2.x) , (r->point_1.y-r->point_2.y));
+}
--- function.h
+++ function.h
@@ -8,17 +8,6 @@ typedef struct{
     coordinates point_2;
 }rectangle;
 
-double rectangleArea( rectangle *r)
-{
-    return fabs(r->point_1.x-r->point_2.x) * fabs(r->point_1.y-r->point_2.y);
-}
-
-double rectanglePerimeter( rectangle *r)
-{
-    return 2 * ( fabs(r->point_1.x-r->point_2.x) + fabs(r->point_1.y-r->point_2.y));
-}
-
-double rectangleDiagonal( rectangle *r)
-{
-    return hypot( (r->point_1.x-r->point_2.x) , (r->point_1.y-r->point_2.y));
-}
+double rectangleArea( rectangle *r);
+double rectanglePerimeter( rectangle *r);
+double rectangleDiagonal( rectangle *r);
